dace80b50a5b846bfaba09576675fc6b5fee1965
Daniel Boesch <you@example.com>
2021-04-22 15:23:08 -0600

    finised base css

---
+++ devmountain/unit-3/3.4/3-4-lab/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="home">Home</h1>
+      </header>
+      <div className='mainBoxParent'>
+        <div className='mainBox'>
+      </div>
+    <div class = 'prevNext'>
+        <h2>{"< Previous"}</h2>
+        <h2>{"Next >"}</h2>
+    </div>
+    
+
+      </div>
+    </div>
+    </body>
+  );
+}
+
+export default App;
