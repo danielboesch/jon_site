0c9a4c3ba6fdf3687e3f4a07bbabe154e5ee8796
Daniel Boesch <you@example.com>
2021-05-06 19:15:40 -0600

    close to done

---
+++ devmountain/unit-5/5.2/responsive-design/src/App.js
@@ -0,0 +1,94 @@
+import { render } from '@testing-library/react';
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      showMenu: false
+    }
+  }
+
+  toggleMenu = () => {
+    this.setState({showMenu: !this.state.showMenu})
+  }
+
+  render(){
+    return (
+      <div className="App">
+              <div class = "page pageOne">
+                  <div class = "pageOneBgColor">
+
+
+
+                      <header>
+                          <img class='headerImg' alt='Start Bootstrap Logo' src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" />
+                          <nav>
+                            <ul class="headerLinks">
+                                <li><b>SERVICES</b></li>
+                                <li><b>PORTFOLIO</b></li>
+                                <li><b>ABOUT</b></li>
+                                <li><b>TEAM</b></li>
+                                <li><b>CONTACT</b></li>
+                            </ul>
+                            <div class={'headerBlank'}>
+                              <span className='menu' onClick={this.toggleMenu}>MENU ☰</span>
+                            </div>
+                            <ul className={`dropLinks ${this.state.showMenu ? "show" : ""}`}>
+                                <li><b>SERVICES</b></li>
+                                <li><b>PORTFOLIO</b></li>
+                                <li><b>ABOUT</b></li>
+                                <li><b>TEAM</b></li>
+                                <li><b>CONTACT</b></li>
+                            </ul>
+                          </nav>
+                      </header>
+
+
+                      
+
+                      <div class="mainBoxBox">
+                      <div class="mainBox">
+                          <div class="mainText">
+                              <h2><i>Welcome To Our Studio!</i></h2>
+                          </div>
+                          <div class="mainText2">
+                              <h1>IT'S NICE TO MEET YOU</h1>
+                          </div>
+                          <div >
+                              <button class="mainButton"><b>TELL ME MORE</b></button>
+                          </div>
+                      </div>
+                      
+                      
+                      
+                      
+                  </div>
+                  </div>
+              </div>
+              <div class = "page pageTwo">
+
+
+              <div class = "pageTwoBg">
+
+                <div class="page2Box">
+                <div class="mainTextPgTwo">
+                      <h2>SERVICES</h2>
+                </div>
+                <div class="subTextPgTwo">
+                      <h2><i>Lorem ipsum dolor sit amet consectetur.</i></h2>
+                </div>
+                </div>
+
+              </div>
+              </div>
+
+
+
+    </div>
+  );
+}
+}
+
+export default App;
